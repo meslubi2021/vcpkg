0d45b4f300c81a130be1f7d70459c3e821e68420
Doug Crawford <doug.crawford@intelight-its.com>
2018-12-17 23:15:06 -0700

    [boost] undef signal_set macro patch for asio
    libevent unfortunately defines the boost signal_set class as a
    macro in event2.h

--- ports/boost-asio/portfile.cmake
+++ ports/boost-asio/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
     REF boost-1.68.0
     SHA512 287aa7b4e37e97df10d196b1b409d38c19a025c18228df1ba9bb7dc19d53c9f51b69699e71b04ddee030710e08cb9c4b34eb08f8284d3fa88a762ed6fd096dbe
     HEAD_REF master
+    PATCHES undefine-signal-set-macro.patch
 )
 
 include(${CURRENT_INSTALLED_DIR}/share/boost-vcpkg-helpers/boost-modular-headers.cmake)

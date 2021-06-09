922cd7f64a4ee1700d3648e3aaafe73900704ee1
Benilda Key <bkey76@gmail.com>
2021-06-09 00:46:23 -0500

    Updated vcpkg-env.el.

--- vcpkg-env.el
+++ vcpkg-env.el
@@ -1,14 +1,10 @@
 (require 'cl-lib)
+(require 'yekneb-constants)
 (require 'yekneb-debug)
 (require 'yekneb-env)
 (require 'yekneb-path-manip)
 
-(defconst vcpkg--32-bit-system-p (not (null (string-match "^i386-.*" system-configuration)))
-  "t if a 32-bit verision of Emacs is in use. nil otherwise.")
-(defconst vcpkg--64-bit-system-p (not (null (string-match "^x86_64-.*" system-configuration)))
-  "t if a 64-bit verision of Emacs is in use. nil otherwise.")
-
-(defconst vcpkg--add-to-path
+(defconst vcpkg--add-to-path-windows
   '(
     "${VCPKG_ROOT}/installed/${Platform}-windows/share/clang"
     "${VCPKG_ROOT}/downloads/tools/7zip-18.1.0-windows/7-Zip.CommandLine.18.1.0/tools"
@@ -31,52 +27,125 @@
     "${ProgramFiles(x86)}/Poedit"
     "${ProgramFiles(x86)}/Poedit/GettextTools/bin"
    )
-  "Directories to add to the path for x86."
+  "Directories to add to the path for Windows."
  )
 
-(let
-  (
-   (yekneb-debug-level yekneb-log-entry)
-   (dic-path "${VCPKG_ROOT}/installed/${Platform}-windows/share/hunspell/dictionaries")
-   (load-file-dir (file-name-directory load-file-name))
-   (platform nil)
-   (tool-dirs nil)
-   (tools-dir "${VCPKG_ROOT}/installed/${Platform}-windows/tools")
-   )
-  (setq
-   platform
-   (cond
-    (vcpkg--32-bit-system-p
-     "x86"
+(defconst vcpkg--add-to-path-linux
+  '(
+    "${VCPKG_ROOT}/downloads/tools/cmake-3.20.2-linux/cmake-3.20.2-linux-x86_64/bin"
+    "${VCPKG_ROOT}/downloads/tools//ninja-1.10.1-linux"
+    "${VCPKG_ROOT}/installed/${Platform}-linux/tools"
+    )
+  "Directories to add to the path for Linux."
+  )
+
+(defun update-environment-for-vcpkg-windows ()
+  "Updates the environment for VCPKG on Windows."
+  (let
+    (
+     (yekneb-debug-level (if init-file-debug yekneb-log-debug yekneb-log-entry))
+     (dic-path "${VCPKG_ROOT}/installed/${Platform}-windows/share/hunspell/dictionaries")
+     (load-file-dir (file-name-directory load-file-name))
+     (platform nil)
+     (tool-dirs nil)
+     (tools-dir "${VCPKG_ROOT}/installed/${Platform}-windows/tools")
      )
-    (vcpkg--64-bit-system-p
-     "x64"
+    (setq
+     platform
+     (cond
+      (yekneb-32-bit-system-p
+       "x86"
+       )
+      (yekneb-64-bit-system-p
+       "x64"
+       )
+      (t
+       nil)
+      )
      )
-    (t
-     nil)
-    )
-   )
-  (when platform
-    (yekneb-log yekneb-log-info "Setting Platform to '%s'." platform)
-    (yekneb-setenv "Platform" platform)
-    )
-  (setq tools-dir (substitute-env-vars tools-dir))
-  (yekneb-log yekneb-log-info "tools-dir is '%s'." tools-dir)
-  (setq dic-path (substitute-env-vars dic-path))
-  (when (file-directory-p dic-path)
-    (yekneb-log yekneb-log-info "Setting DICPATH to '%s'." dic-path)
-    (yekneb-setenv "DICPATH" dic-path)
-    )
-  (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path to the path.")
-  (yekneb-add-dirs-to-path vcpkg--add-to-path t t load-file-dir)
-  (yekneb-log yekneb-log-info "Adding the directories in tool-dirs to the path.")
-  (setq tool-dirs (directory-files tools-dir))
-  (dolist (tool-dir tool-dirs)
-    (when (and (not (string= tool-dir ".")) (not (string= tool-dir "..")))
-      (setq tool-dir (expand-file-name tool-dir tools-dir))
-      (when (file-directory-p tool-dir)
-        (yekneb-add-to-path tool-dir t)
+    (when platform
+      (yekneb-log yekneb-log-info "Setting Platform to '%s'." platform)
+      (yekneb-setenv "Platform" platform)
+      )
+    (setq tools-dir (substitute-env-vars tools-dir))
+    (yekneb-log yekneb-log-info "tools-dir is '%s'." tools-dir)
+    (setq dic-path (substitute-env-vars dic-path))
+    (yekneb-log yekneb-log-info "dic-path is '%s'." dic-path)
+    (when (file-directory-p dic-path)
+      (yekneb-log yekneb-log-info "Setting DICPATH to '%s'." dic-path)
+      (yekneb-setenv "DICPATH" dic-path)
+      )
+    (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path-windows to the path.")
+    (yekneb-add-dirs-to-path vcpkg--add-to-path-windows t t load-file-dir)
+    (yekneb-log yekneb-log-info "Adding the directories in tool-dirs to the path.")
+    (when (file-directory-p tools-dir)
+      (setq tool-dirs (directory-files tools-dir))
+      (dolist (tool-dir tool-dirs)
+        (when (and (not (string= tool-dir ".")) (not (string= tool-dir "..")))
+          (setq tool-dir (expand-file-name tool-dir tools-dir))
+          (when (file-directory-p tool-dir)
+            (yekneb-add-to-path tool-dir t)
+            )
+          )
         )
       )
     )
   )
+
+(defun update-environment-for-vcpkg-linux ()
+  "Updates the environment for VCPKG on Linux."
+  (let
+    (
+     (yekneb-debug-level (if init-file-debug yekneb-log-debug yekneb-log-entry))
+     (load-file-dir (file-name-directory load-file-name))
+     (platform nil)
+     (tool-dirs nil)
+     (tools-dir "${VCPKG_ROOT}/installed/${Platform}-linux/tools")
+     )
+    (setq
+     platform
+     (cond
+      (yekneb-32-bit-system-p
+       "x86"
+       )
+      (yekneb-64-bit-system-p
+       "x64"
+       )
+      (t
+       nil)
+      )
+     )
+    (when platform
+      (yekneb-log yekneb-log-info "Setting Platform to '%s'." platform)
+      (yekneb-setenv "Platform" platform)
+      )
+    (setq tools-dir (substitute-env-vars tools-dir))
+    (yekneb-log yekneb-log-info "tools-dir is '%s'." tools-dir)
+    (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path-linux to the path.")
+    (yekneb-add-dirs-to-path vcpkg--add-to-path-linux t t load-file-dir)
+    (yekneb-log yekneb-log-info "Adding the directories in tool-dirs to the path.")
+    (when (file-directory-p tools-dir)
+      (setq tool-dirs (directory-files tools-dir))
+      (dolist (tool-dir tool-dirs)
+        (when (and (not (string= tool-dir ".")) (not (string= tool-dir "..")))
+          (setq tool-dir (expand-file-name tool-dir tools-dir))
+          (when (file-directory-p tool-dir)
+            (yekneb-add-to-path tool-dir t)
+            )
+          )
+        )
+      )
+    )
+  )
+
+(defun update-environment-for-vcpkg ()
+  "Updates the environment for VCPKG."
+  (when (eq system-type 'windows-nt)
+    (update-environment-for-vcpkg-windows)
+    )
+  (when (eq system-type 'gnu/linux)
+    (update-environment-for-vcpkg-linux)
+    )
+  )
+
+(update-environment-for-vcpkg)

3b7a3cacacf7ecfe440b6e000d1780099ab6c478
Francis Lemaire <lemairefrancis10@gmail.com>
2021-07-18 23:55:53 -0400

    WIP

--- ports/wasmer/portfile.cmake
+++ ports/wasmer/portfile.cmake
@@ -32,45 +32,45 @@
 # # Specifies if the port install should fail immediately given a condition
 # vcpkg_fail_port_install(MESSAGE "wasmer currently only supports Linux and Mac platforms" ON_TARGET "Windows")
 
-# vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
-#     REPO wasmerio/wasmer
-#     REF 1.0.2
-#     SHA512 f8058cbd5a8a807cd84b4a839c87ff76dae5475c655e804b27262cd5cb22ddb6c43da3a01b6cdbed29502afc38aa9ee589022f67fab27b189453f2500478c4a9
-#     HEAD_REF master
+vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
+    REPO wasmerio/wasmer
+    REF 1.0.2
+    SHA512 f8058cbd5a8a807cd84b4a839c87ff76dae5475c655e804b27262cd5cb22ddb6c43da3a01b6cdbed29502afc38aa9ee589022f67fab27b189453f2500478c4a9
+    HEAD_REF master
+)
+# message(STATUS "Archive: ${VCPKG_TARGET_IS_LINUX} and ${VCPKG_TARGET_ARCHITECTURE}")
+
+# if(VCPKG_TARGET_IS_LINUX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+# vcpkg_download_distfile(ARCHIVE
+#     URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-linux-amd64.tar.gz"
+#     FILENAME "wasmer.tar.gz"
+#     SHA512 a0a96413f9117861d2ff6f99460b20f4516825adafaac28cf08cc8ec20a0b35e72bd08832f6501778b78c3aae43e1870bbda5df07918a0e0d22c78d33aa1cbb7
 # )
-message(STATUS "Archive: ${VCPKG_TARGET_IS_LINUX} and ${VCPKG_TARGET_ARCHITECTURE}")
+# elseif(VCPKG_TARGET_IS_LINUX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "ARM")
+# vcpkg_download_distfile(ARCHIVE
+#     URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-linux-aarch64.tar.gz"
+#     FILENAME "wasmer.tar.gz"
+#     SHA512 4781F48823842E244A298A8068857310CDBEFB22114319267A05E7BBF637D5A2F1BB89D4A1825117E27E56F7FA8B87A8486685DDE1A8AB8860BCE8FDFDDEAC72
+# )
+# elseif(VCPKG_TARGET_IS_OSX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+# vcpkg_download_distfile(ARCHIVE
+#     URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-darwin-amd64.tar.gz"
+#     FILENAME "wasmer.tar.gz"
+#     SHA512 09DC1A729C5A152D91734F63D97641C4DF7F8DD71F02FB79F59852896183D5003F62B93E01A72ACA52FD8A5D1DD8E4E69A854ECD926AB354F2824E024E05EDCE
+# )
+# elseif(VCPKG_TARGET_IS_OSX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "ARM")
+# vcpkg_download_distfile(ARCHIVE
+#     URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-darwin-arm64.tar.gz"
+#     FILENAME "wasmer.tar.gz"
+#     SHA512 B11DBADB4E9A0476761D9F1F9BEACAD560FD08A66FFB698DBD1FD66BD7ECD24DAE3BCA240DE32894F21A0BABEA5B71C262A65A4DC5CF4D8469DD75AAB939214B
+# )
+# endif()
 
-if(VCPKG_TARGET_IS_LINUX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-linux-amd64.tar.gz"
-    FILENAME "wasmer.tar.gz"
-    SHA512 a0a96413f9117861d2ff6f99460b20f4516825adafaac28cf08cc8ec20a0b35e72bd08832f6501778b78c3aae43e1870bbda5df07918a0e0d22c78d33aa1cbb7
-)
-elseif(VCPKG_TARGET_IS_LINUX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "ARM")
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-linux-aarch64.tar.gz"
-    FILENAME "wasmer.tar.gz"
-    SHA512 4781F48823842E244A298A8068857310CDBEFB22114319267A05E7BBF637D5A2F1BB89D4A1825117E27E56F7FA8B87A8486685DDE1A8AB8860BCE8FDFDDEAC72
-)
-elseif(VCPKG_TARGET_IS_OSX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-darwin-amd64.tar.gz"
-    FILENAME "wasmer.tar.gz"
-    SHA512 09DC1A729C5A152D91734F63D97641C4DF7F8DD71F02FB79F59852896183D5003F62B93E01A72ACA52FD8A5D1DD8E4E69A854ECD926AB354F2824E024E05EDCE
-)
-elseif(VCPKG_TARGET_IS_OSX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "ARM")
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/wasmerio/wasmer/releases/download/1.0.2/wasmer-darwin-arm64.tar.gz"
-    FILENAME "wasmer.tar.gz"
-    SHA512 B11DBADB4E9A0476761D9F1F9BEACAD560FD08A66FFB698DBD1FD66BD7ECD24DAE3BCA240DE32894F21A0BABEA5B71C262A65A4DC5CF4D8469DD75AAB939214B
-)
-endif()
-
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    NO_REMOVE_ONE_LEVEL
-)
+# vcpkg_extract_source_archive_ex(
+#     OUT_SOURCE_PATH SOURCE_PATH
+#     ARCHIVE ${ARCHIVE}
+#     NO_REMOVE_ONE_LEVEL
+# )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 

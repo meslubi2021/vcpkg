dbc4711d74ff082ba9ee2b4f8beaa73ec19dc542
Benilda Key <bkey76@gmail.com>
2022-12-28 08:42:29 -0600

    Updated path and dotnet version.

--- update-env.bat
+++ update-env.bat
@@ -6,7 +6,7 @@ call :GetBatchFileDirectory _MyDir
 call :SetOPT
 if not defined OPT goto :EOF
 
-set DOTNET_VERSION=6.0.400
+set DOTNET_VERSION=7.0.100
 set HOME=%USERPROFILE%\Home
 set JDK_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.4.101-hotspot
 set JDK_HOME_X64=C:\Program Files\Eclipse Adoptium\jdk-17.0.4.101-hotspot
@@ -26,7 +26,7 @@ for %%a in (
 "%_MyDir%\installed\%Platform%-windows\tools\sassc\bin"
 "%_MyDir%\installed\%Platform%-windows\tools\tcl\bin"
 "%_MyDir%\downloads\tools\7z\Files\7-Zip"
-"%_MyDir%\downloads\tools\cmake-3.24.0-windows\cmake-3.24.0-windows-i386\bin"
+"%_MyDir%\downloads\tools\cmake-3.25.0-windows\cmake-3.25.0-windows-i386\bin"
 "%_MyDir%\downloads\tools\jom\jom-1.1.3"
 "%_MyDir%\downloads\tools\nasm\nasm-2.15.05"
 "%_MyDir%\downloads\tools\nasm\nasm-2.15.05\rdoff"
@@ -34,7 +34,7 @@ for %%a in (
 "%_MyDir%\downloads\tools\perl\5.32.1.1\c\i686-w64-mingw32\bin"
 "%_MyDir%\downloads\tools\perl\5.32.1.1\perl\site\bin"
 "%_MyDir%\downloads\tools\perl\5.32.1.1\perl\bin"
-"%_MyDir%\downloads\tools\python\python-3.10.5-%Platform%"
+"%_MyDir%\downloads\tools\python\python-3.10.7-%Platform%"
 "%_MyDir%\downloads\tools\win_bison\2.5.25"
 "%_MyDir%\downloads\tools\win_flex\2.5.25"
 "%ProgramW6432%\Beyond Compare 4"
@@ -59,7 +59,7 @@ for %%a in (
 "%ProgramFiles(x86)%\Poedit"
 "%ProgramFiles(x86)%\Poedit\GettextTools\bin"
 "%LOCALAPPDATA%\Programs\Microsoft VS Code"
-"%OPT%\Apache-Subversion-1.14.1\bin"
+"%OPT%\Apache-Subversion-1.14.2\bin"
 "%OPT%\bin\X64"
 "%OPT%\bin\X86"
 "%OPT%\ExamDiff"

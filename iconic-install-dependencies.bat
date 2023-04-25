3f77f30cabbc448658d2f96a05578704c889df9e
Andreas <andreas.vralstad@gmail.com>
2023-04-25 10:26:16 +0200

    Added libzippp, and put all packages on a single line since previous dependency bug seems to have been solved

--- iconic-install-dependencies.bat
+++ iconic-install-dependencies.bat
@@ -1,3 +1,2 @@
 @echo off
-.\vcpkg.exe install --triplet x64-windows --binarysource=clear wxwidgets[webview]
-.\vcpkg.exe install --triplet x64-windows --binarysource=clear glew glfw3 eigen3 proj libgeotiff clfft boost opencl pcl liblas cuda ffmpeg[opencl,nvcodec,webp,zlib,vorbis,lzma,openh264] openimageio[gif,libraw] cryptopp 
+.\vcpkg.exe install --triplet x64-windows --binarysource=clear glew glfw3 eigen3 proj libgeotiff clfft boost opencl pcl liblas cuda ffmpeg[opencl,nvcodec,webp,zlib,vorbis,lzma,openh264] openimageio[gif,libraw] cryptopp libzippp wxwidgets[webview]

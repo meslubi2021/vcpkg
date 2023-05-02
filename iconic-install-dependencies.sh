56d9b763ccdcdd5789c0bf268a09ae465152ce73
Andreas <andreas.vralstad@gmail.com>
2023-05-02 09:57:40 +0200

    Added libtess

--- iconic-install-dependencies.sh
+++ iconic-install-dependencies.sh
@@ -1,3 +1,2 @@
 #!/bin/sh -e
-./vcpkg install --triplet x64-linux --binarysource=clear wxwidgets[webview]
-./vcpkg install --triplet x64-linux --binarysource=clear glew glfw3 eigen3 proj libgeotiff clfft boost opencl pcl liblas cuda ffmpeg[opencl,nvcodec,webp,zlib,vorbis,lzma,openh264] openimageio[gif,libraw] cryptopp 
+./vcpkg install --triplet x64-linux --binarysource=clear glew glfw3 eigen3 proj libgeotiff clfft boost opencl pcl liblas cuda ffmpeg[opencl,nvcodec,webp,zlib,vorbis,lzma,openh264] openimageio[gif,libraw] cryptopp libzippp wxwidgets[webview] libtess2

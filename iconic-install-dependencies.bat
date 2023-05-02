343bcc17483088f8dfc048a46ea51f3efadcdc17
Andreas <andreas.vralstad@gmail.com>
2023-05-02 10:11:31 +0200

    Add wxwidgets examples

--- iconic-install-dependencies.bat
+++ iconic-install-dependencies.bat
@@ -1,2 +1,2 @@
 @echo off
-.\vcpkg.exe install --triplet x64-windows --binarysource=clear glew glfw3 eigen3 proj libgeotiff clfft boost opencl pcl liblas cuda ffmpeg[opencl,nvcodec,webp,zlib,vorbis,lzma,openh264] openimageio[gif,libraw] cryptopp libzippp wxwidgets[webview] libtess2
+.\vcpkg.exe install --triplet x64-windows --binarysource=clear glew glfw3 eigen3 proj libgeotiff clfft boost opencl pcl liblas cuda ffmpeg[opencl,nvcodec,webp,zlib,vorbis,lzma,openh264] openimageio[gif,libraw] cryptopp libzippp wxwidgets[webview,example] libtess2

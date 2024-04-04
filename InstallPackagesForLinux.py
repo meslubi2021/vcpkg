d02cfad79cdee088bd7524d2d70b297b471bcabf
Benilda Key <bkey76@gmail.com>
2024-04-04 06:05:55 -0500

    Update package list.

--- InstallPackagesForLinux.py
+++ InstallPackagesForLinux.py
@@ -4,66 +4,56 @@ import os
 import subprocess
 
 packageList = [
-    ([
-        'abseil[core,cxx17]',
-        'aixlog',
-        'akali',
-        'allegro5',
-        'angelscript[addons,core]',
-        'antlr4',
-        'apr-util',
-        'apr[core,private-headers]',
-        'arabica',
-        'asmjit',
-        'boost-locale[icu]',
-        'boost-odeint[mpi]',
-        'boost-regex[icu]',
-        'boost[mpi]',
-        'icu',
-        'mpi',
-        'poco',
-        'poco[sqlite3]',
-        'sqlite-modern-cpp',
-        'sqlite3',
-        'sqlite3[tool]',
-        'sqlitecpp',
-    ], False),
+  ([
+    'boost-asio[core,ssl]',
+    'boost-locale[core,icu]',
+    'boost-mpi[core]',
+    'boost-odeint[core,mpi]',
+    'boost-regex[core,icu]',
+    'boost[core,mpi]',
+    'icu[core,tools]',
+    'mp3lame',
+    'mpi',
+    'sqlite3[core,json1,tool,zlib]',
+    'sqlitecpp',
+    'strtk'
+  ], False),
 ]
 
 def InstallPackagesWorker(packages, triplet, recurse):
-    args = []
-    args.append("./vcpkg")
-    args.append("install")
-    args.extend(packages)
-    args.append("--triplet")
-    args.append(triplet)
-    if (recurse):
-        args.append("--recurse")
-    try:
-        seperator = ' '
-        print("Calling '%s'." % seperator.join(args))
-        subprocess.check_call(args)
-        return True
-    except subprocess.CalledProcessError as err:
-        return False
-    except OSError as err:
-        return False
+  args = []
+  args.append("./vcpkg")
+  args.append("install")
+  args.extend(packages)
+  args.append("--triplet")
+  args.append(triplet)
+  if (recurse):
+    args.append("--recurse")
+  try:
+    seperator = ' '
+    print("Calling '%s'." % seperator.join(args))
+    subprocess.check_call(args)
+    return True
+  except subprocess.CalledProcessError as err:
+    return False
+  except OSError as err:
+    return False
 
 def InstallPackages(packages, recurse):
-    print()
-    print("################################################################################")
-    print("Installing packages: %s" % packages)
-    print("################################################################################")
-    print()
-    ret = InstallPackagesWorker(packages, "x64-linux", recurse)
-    print()
-    return ret
+  print()
+  print("################################################################################")
+  print("Installing packages: %s" % packages)
+  print("################################################################################")
+  print()
+  ret = InstallPackagesWorker(packages, "x64-linux", recurse)
+  print()
+  return ret
 
 def InstallPackagesInPackageList():
-    for package in packageList:
-         ret = InstallPackages(package[0], package[1])
-         if ret == False:
-             return False
-    return True
+  for package in packageList:
+    ret = InstallPackages(package[0], package[1])
+    if ret == False:
+      return False
+  return True
 
 InstallPackagesInPackageList()
